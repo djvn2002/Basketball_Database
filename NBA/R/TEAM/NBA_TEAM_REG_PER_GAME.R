49cfae93c8e6063c28f855fc14a04608efa7005f
David Vialpando-Nielsen <djvialpando@gmail.com>
2024-09-05 16:58:16 -0600

    Traditional team stats for NBA

---
+++ NBA/R/TEAM/NBA_TEAM_REG_PER_GAME.R
@@ -0,0 +1,50 @@
+# Author: David Vialpando-Nielsen
+# Date Made: 9/5/2024
+# Latest Update: 9/5/2024
+
+# This file will run based on the assumption that you have already ran this file:
+# NBA_TEAM_REG_TOTAL.R
+
+# The purpose of this file is to grab an aggregate of seasonal per game data by team.
+
+# Library Packages
+library(tidyverse)
+
+# Load NBA_PLAYER_REG_TOTAL
+team_fp <- "C:/Users/djvia/OneDrive/Documents/Blog Website/Basketball_Database/NBA/TEAM/REGULAR SEASON"
+load(file = file.path(team_fp,"NBA_TEAM_REG_TOTAL.rda"))
+
+# Aggregating based by dividing games played for each player by season
+nba_team_reg_per_game <- nba_team_reg_total %>%
+  group_by( `Team Name`, `Team Abbr.`, Season, URL) %>%
+  summarise( G = sum(G, na.rm = TRUE),
+             `MP/G` = round(sum(MP, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `FG/G` = round(sum(FG, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `FGA/G` = round(sum(FGA, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `FG%` = round(sum(FG, na.rm = TRUE) / sum(FGA, na.rm = TRUE), 3),
+             `3P/G` = round(sum(`3P`, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `3PA/G` = round(sum(`3PA`, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `3P%` = round(sum(`3P`, na.rm = TRUE) / sum(`3PA`, na.rm = TRUE), 3),
+             `2P/G` = round(sum(`2P`, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `2PA/G` = round(sum(`2PA`, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `2P%` = round(sum(`2P`, na.rm = TRUE) / sum(`2PA`, na.rm = TRUE), 3),
+             `eFG%` = round((sum(FG, na.rm = TRUE) + (0.5 * sum(`3P`, na.rm = TRUE))) / sum(FGA, na.rm = TRUE), 3),
+             `FT/G` = round(sum(FT, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `FTA/G` = round(sum(FTA, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `FT%` = round(sum(FT, na.rm = TRUE) / sum(FTA, na.rm = TRUE), 3),
+             `ORB/G` = round(sum(ORB, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `DRB/G` = round(sum(DRB, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `TRB/G` = round(sum(TRB, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `AST/G` = round(sum(AST, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `STL/G` = round(sum(STL, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `BLK/G` = round(sum(BLK, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `TOV/G` = round(sum(TOV, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `PF/G` = round(sum(PF, na.rm = TRUE) / sum(G, na.rm = TRUE), 2),
+             `PTS/G` = round(sum(PTS, na.rm = TRUE) / sum(G, na.rm = TRUE), 2)) %>%
+  select(`Team Name`, `Team Abbr.`, Season, G, `MP/G`, `FG/G`, `FGA/G`, `FG%`, 
+         `3P/G`, `3PA/G`, `3P%`, `2P/G`, `2PA/G`, `2P%`, `eFG%`,`FT/G`, `FTA/G`, 
+         `FT%`, `ORB/G`, `DRB/G`, `TRB/G`, `AST/G`, `STL/G`, `BLK/G`, `TOV/G`, 
+         `PF/G`, `PTS/G`)
+
+# Save per game data frame to a rda file
+save(nba_team_reg_per_game,file = file.path(team_fp,"NBA_TEAM_REG_PER_GAME.rda"))

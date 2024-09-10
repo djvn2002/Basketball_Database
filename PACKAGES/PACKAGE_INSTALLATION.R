4c0e66bb29473eb5a71c55368610c05b0a74795c
David Vialpando-Nielsen <djvialpando@gmail.com>
2024-09-09 20:54:22 -0600

    Scrape file for NBA stats per 36 minutes.

--- PACKAGES/PACKAGE_INSTALLATION.R
+++ PACKAGES/PACKAGE_INSTALLATION.R
@@ -1,6 +1,6 @@
 # Author: David Vialpando-Nielsen
 # Date Made: 8/23/2024
-# Latest Update: 8/25/2024
+# Latest Update: 9/7/2024
 
 install.packages("tidyverse")
 install.packages("httr")
@@ -15,3 +15,8 @@ install.packages("bslib")
 install.packages("sportyR")
 install.packages("ggthemes")
 install.packages("zoo")
+install.packages("furr")
+install.packages("RSelenium")
+install.packages("netstat")
+install.packages("wdman")
+install.packages("progressr")

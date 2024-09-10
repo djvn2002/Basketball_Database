aaba786833bb6640d13ee3266faab67b895fdea5
David Vialpando-Nielsen <djvialpando@gmail.com>
2024-09-09 23:19:17 -0600

    Shot RDA Files Updated at Mon 09/09/2024 23:19:17.61

---
+++ SHOT APP/BATCH FILES/run_shot_data.bat
@@ -0,0 +1,51 @@
+@echo off
+
+:: Clear log file at the start of each run
+echo. > "C:\Users\djvia\OneDrive\Documents\Blog Website\Basketball_Database\SHOT APP\BATCH FILES\log.txt"
+
+:: Log file location
+set log_file="C:\Users\djvia\OneDrive\Documents\Blog Website\Basketball_Database\SHOT APP\BATCH FILES\log.txt"
+
+:: WNBA Shots Script
+echo Running WNBA Shots Script >> %log_file%
+"C:\Program Files\R\R-4.4.1\bin\Rscript.exe" "C:\Users\djvia\OneDrive\Documents\Blog Website\Basketball_Database\SHOT APP\WNBA\wnba_shots.R" >> %log_file% 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo WNBA Shots Script failed at %time% on %date% >> %log_file%
+    exit /b 1
+)
+echo WNBA Shots Script completed at %time% on %date% >> %log_file%
+
+:: NBA Shots Script
+echo Running NBA Shots Script >> %log_file%
+"C:\Program Files\R\R-4.4.1\bin\Rscript.exe" "C:\Users\djvia\OneDrive\Documents\Blog Website\Basketball_Database\SHOT APP\NBA\nba_shots.R" >> %log_file% 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo NBA Shots Script failed at %time% on %date% >> %log_file%
+    exit /b 1
+)
+echo NBA Shots Script completed at %time% on %date% >> %log_file%
+
+:: MNCAA Shots Script
+echo Running MNCAA Shots Script >> %log_file%
+"C:\Program Files\R\R-4.4.1\bin\Rscript.exe" "C:\Users\djvia\OneDrive\Documents\Blog Website\Basketball_Database\SHOT APP\NCAA\mens_college_data.R" >> %log_file% 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo MNCAA Shots Script failed at %time% on %date% >> %log_file%
+    exit /b 1
+)
+echo MNCAA Shots Script completed at %time% on %date% >> %log_file%
+
+:: WNCAA Shots Script
+echo Running WNCAA Shots Script >> %log_file%
+"C:\Program Files\R\R-4.4.1\bin\Rscript.exe" "C:\Users\djvia\OneDrive\Documents\Blog Website\Basketball_Database\SHOT APP\NCAA\womens_college_data.R" >> %log_file% 2>&1
+if %ERRORLEVEL% neq 0 (
+    echo WNCAA Shots Script failed at %time% on %date% >> %log_file%
+    exit /b 1
+)
+echo WNCAA Shots Script completed at %time% on %date% >> %log_file%
+
+echo All scripts have completed successfully at %time% on %date% >> %log_file%
+
+cd "C:\Users\djvia\OneDrive\Documents\Blog Website\Basketball_Database\SHOT APP"
+git add .
+git commit -m "Shot RDA Files Updated at %date% %time%"
+git push origin main
+git pull origin main
